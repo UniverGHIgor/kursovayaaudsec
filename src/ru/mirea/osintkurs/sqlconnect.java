977cf2165625f61d62b350171a37a4f82f7b8b2b
UniverGHIgor <mkhvigr@gmail.com>
2022-05-28 17:36:45 +0300

    add sql

---
+++ src/ru/mirea/osintkurs/sqlconnect.java
@@ -0,0 +1,26 @@
+package ru.mirea.osintkurs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class sqlconnect {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main() {
+        String connectionUrl =
+                "jdbc:sqlserver://mysql.j06318538.myjino.ru:3306;"
+                        + "database=j06318538;"
+                        + "user=j06318538;"
+                        + "encrypt=false;"
+                ;
+
+        String insertSql = "INSERT INTO j06318538.Popular (nickname, mail, password) VALUES "
+                + "('volodka', 'volodya2006@gmail.com', 'volodyakrutoy');";
+
+        ResultSet resultSet = null;
+
+    }
+}
