df68dd4c8c50790b90b9a55cd030cb2b336a1792
UniverGHIgor <mkhvigr@gmail.com>
2022-05-28 20:05:58 +0300

    fix sql

--- src/ru/mirea/osintkurs/sqlconnect.java
+++ src/ru/mirea/osintkurs/sqlconnect.java
@@ -5,22 +5,30 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.*;
+
 
 public class sqlconnect {
     // Connect to your database.
     // Replace server name, username, and password with your credentials
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+    }
     public static void main() {
-        String connectionUrl =
-                "jdbc:sqlserver://mysql.j06318538.myjino.ru:3306;"
-                        + "database=j06318538;"
-                        + "user=j06318538;"
-                        + "encrypt=false;"
-                ;
 
-        String insertSql = "INSERT INTO j06318538.Popular (nickname, mail, password) VALUES "
-                + "('volodka', 'volodya2006@gmail.com', 'volodyakrutoy');";
+        String url = "jdbc:mysql://mysql.j06318538.myjino.ru:3306/j06318538";
+        String username = "j06318538";
+        String password = "kursovaya";
 
-        ResultSet resultSet = null;
+        System.out.println("Connecting database...");
 
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+
+            String insertSql = "INSERT INTO j06318538.popular (nickname, mail, password) VALUES "
+                    + "('volodka', 'volodya2006@gmail.com', 'volodyakrutoy');";
     }
 }
