2225bc7af750a281c36271347a6fecc82d4e4141
Skif926 <skif926@mail.ru>
2022-05-29 15:24:19 +0300

    redact 0.3

--- src/ru/mirea/osintkurs/SQLconnect.java
+++ src/ru/mirea/osintkurs/SQLconnect.java
@@ -2,34 +2,24 @@ package ru.mirea.osintkurs;
 
 //import java.sql.Connection;
 //import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
 import java.sql.*;
 
-
-public class sqlconnect {
-
+public class SQLconnect {
     // Connect to your database.
     // Replace server name, username, and password with your credentials
 
 //    public static void jdbc(String[] args) throws Exception {
 //        Class.forName("com.mysql.jdbc.Driver");
 //    }
-    public static void Startconnect() {
-        String url      = "jdbc:mysql://mysql.j06318538.myjino.ru:3306/j06318538",
-               username = "j06318538",
-               pass     = "kursovaya";
-
+    public static void Startconnect(String url, String username, String pass) {
         System.out.println("Connecting database...");
-
-        try (Connection conn = DriverManager.getConnection(url, username, pass)) {
+        try (Connection connection = DriverManager.getConnection(url, username, pass)) {
             System.out.println("Database connected!");
-
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect the database!", e);
         }
-
     }
 }
