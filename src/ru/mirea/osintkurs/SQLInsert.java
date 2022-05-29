b2e9a5de5b89213210bce86c2e4940836d15faf6
Skif926 <skif926@mail.ru>
2022-05-29 13:40:05 +0300

    redact 0.1

---
+++ src/ru/mirea/osintkurs/SQLInsert.java
@@ -0,0 +1,62 @@
+package ru.mirea.osintkurs;
+import java.sql.*;
+import java.util.*;
+import java.sql.PreparedStatement;
+public class SQLInsert {
+
+    public static void Startinsert() {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://mysql.j06318538.myjino.ru:3306/j06318538", "j06318538", "kursovaya");
+            Statement stmt = null;
+            Scanner scn = new Scanner(System.in);
+            String nickname = null, mail = null, password = null;
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+                System.out.print("Enter your nickname: ");
+                nickname = scn.nextLine();
+
+                System.out.print("Enter your mail: ");
+                mail = scn.nextLine();
+
+                System.out.print("Enter your password: ");
+                password = scn.nextLine();
+
+
+
+                System.out.print("\nInserting your data into table...");
+                stmt = conn.createStatement();
+
+                String insertSql = "INSERT INTO j06318538.Popular (nickname, mail, password) VALUES "
+                        + "('"+nickname+"','"+mail+"','"+password+"');";
+
+
+                PreparedStatement statement = conn.prepareStatement(insertSql);
+                statement.executeUpdate(insertSql);
+
+
+                System.out.println(" SUCCESS!\n");
+
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (stmt != null)
+                        conn.close();
+                } catch (SQLException se) {
+                }
+                try {
+                    if (conn != null)
+                        conn.close();
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }
+            }
+            System.out.println("Thank you for your patronage!");
+        }
+        catch (SQLException e) {}
+    }
+}
